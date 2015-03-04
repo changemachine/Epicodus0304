2a3315433746e813313543f338cd705daad874e0
Liz Beacham & James Nielson <student@epicodus.com>
2015-03-04 12:02:42 -0800

    Add session superglobal monstrosity and form

--- app/app.php
+++ app/app.php
@@ -2,21 +2,40 @@
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/todo.php";
 
+    session_start();
+    if(empty($_SESSION['list_of_tasks'])){
+        $_SESSION['list_of_tasks'] = array();
+    }
+
     $app = new Silex\Application();
 
     $app->get("/", function() {
-        $test_task = new ToDo("Learn PHP.");
-        $task2 = new ToDo("Learn Drupal.");
-        $third_task = new ToDo("Visit France.");
+        // $test_task = new ToDo("Learn PHP.");
+        // $task2 = new ToDo("Learn Drupal.");
+        // $third_task = new ToDo("Visit France.");
+        //
+        // $list_of_tasks = array($test_task, $task2, $third_task);
 
-        $list_of_tasks = array($test_task, $task2, $third_task);
+
 
         $output = "";
 
-        foreach ($list_of_tasks as $task) {
+        // foreach ($list_of_tasks as $task) {
+        //     $output = $output . "<p>" . $task->getDescription() . "</p>";
+        // }
+
+        foreach (ToDo::getAll() as $task) {
             $output = $output . "<p>" . $task->getDescription() . "</p>";
         }
 
+        $output = $output."</ul>
+            <form action='/tasks' method='post'>
+                <label for='description'>To Do:</label>
+                <input id='description' name='description' type='text'>
+                <button type='submit'>Submit</button>
+            </form>
+        ";
+        
         return $output;
 
     });
--- src/todo.php
+++ src/todo.php
@@ -16,6 +16,16 @@
         {
             return $this->description;
         }
+
+        function save()
+        {
+            array_push ($_SESSION['list_of_tasks'], $this);
+        }
+
+        static function getAll()
+        {
+            return $_SESSION['list_of_tasks'];
+        }
     }
 
 ?>
